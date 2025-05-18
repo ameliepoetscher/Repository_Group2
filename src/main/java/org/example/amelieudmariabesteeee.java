ed46c32931693718363bd3541607b9309b3ad873
Dein Name <deine.email@beispiel.com>
2025-05-18 21:20:37 +0200

    test

---
+++ src/main/java/org/example/amelieudmariabesteeee.java
@@ -0,0 +1,48 @@
+/*
+ * Created by JFormDesigner on Sun May 18 21:19:26 CEST 2025
+ */
+
+package org.example;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author amaim
+ */
+public class amelieudmariabesteeee extends JFrame {
+    public amelieudmariabesteeee() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Amaim Mumtaz Rathor
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Amaim Mumtaz Rathor
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
