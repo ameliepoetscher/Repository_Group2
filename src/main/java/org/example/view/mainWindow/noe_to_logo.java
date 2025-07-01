d64fe2a6eb1488f405407866d79df9d419be0d83
ami <fhb232606@fh-vie.ac.at>
2025-07-01 10:22:51 +0200

    final

--- src/main/java/org/example/view/mainWindow/noe_to_logo.java
+++ src/main/java/org/example/view/mainWindow/noe_to_logo.java
@@ -23,7 +23,7 @@ public class noe_to_logo extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - Amaim Mumtaz Rathor
+        // Generated using JFormDesigner Educational license - Amelie Pötscher (fhb232606)
         button1 = new JButton();
         label1 = new JLabel();
         separator1 = new JPopupMenu.Separator();
@@ -34,7 +34,6 @@ public class noe_to_logo extends JFrame {
         //---- button1 ----
         button1.setText("Log In");
         button1.setBackground(new Color(0x3399ff));
-        button1.setForeground(Color.white);
         button1.setFont(button1.getFont().deriveFont(button1.getFont().getStyle() | Font.BOLD));
         button1.addActionListener(e -> loginbutton2(e));
 
@@ -62,13 +61,13 @@ public class noe_to_logo extends JFrame {
                 .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
                     .addGroup(contentPaneLayout.createParallelGroup()
                         .addGroup(contentPaneLayout.createSequentialGroup()
-                            .addContainerGap(48, Short.MAX_VALUE)
+                            .addContainerGap(54, Short.MAX_VALUE)
                             .addComponent(label1, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
                             .addGap(42, 42, 42))
                         .addGroup(contentPaneLayout.createSequentialGroup()
                             .addGap(81, 81, 81)
                             .addComponent(separator1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 122, Short.MAX_VALUE)))
                     .addComponent(button1)
                     .addGap(85, 85, 85))
         );
@@ -78,7 +77,7 @@ public class noe_to_logo extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - Amaim Mumtaz Rathor
+    // Generated using JFormDesigner Educational license - Amelie Pötscher (fhb232606)
     private JButton button1;
     private JLabel label1;
     private JPopupMenu.Separator separator1;
